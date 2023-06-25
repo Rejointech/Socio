07bdc4eeb270d7322a9c5f228a33f944c22c7212
Prajjwal Kumar <prajjwal.kumar@juspay.in>
2023-06-25 23:55:14 +0530

    feat(router): Basic test implementations (#14)
    Co-authored-by: Chethan <chethan.rao@juspay.in>

--- crates/router/src/lib.rs
+++ crates/router/src/lib.rs
@@ -4,7 +4,7 @@ use actix_web::{get, App, HttpServer, Responder};
 
 #[get("/health")]
 async fn greet() -> impl Responder {
-    format!("Health is good ! ")
+    "Health is good !".to_string()
 }
 
 #[actix_web::main]
@@ -13,4 +13,8 @@ pub async fn start_server() -> std::io::Result<()> {
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
+}
+
+pub fn sample_func() -> i32 {
+    21
 }
---
+++ crates/router/tests/health.rs
@@ -0,0 +1,6 @@
+use router;
+
+#[test]
+pub fn sample_test() {
+    assert_eq!(21, router::sample_func());
+}
